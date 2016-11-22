28ac9ceabbc5f867b5bdb9814ec669d4699c76ab
yifan713 <yifanzhang713@yahoo.com>
2016-11-21 23:42:09 -0600

    hello world

---
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Zhang Yifan on 11/21/16.
+//  Copyright © 2016 Yifan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("helloworld")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
